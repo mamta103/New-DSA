7ff5588347d196701cf7b09f015bf26a79ad8288
Mamta Kumari <32190445+mamta103@users.noreply.github.com>
2025-02-07 12:14:08 +0530

    Update AddMonthsExample.java

--- src/java8/AddMonthsExample.java
+++ src/java8/AddMonthsExample.java
@@ -1,9 +1,16 @@
-package java8; import java.time.LocalDate;
+package java8;
+
+import java.time.LocalDate;
 
 public class AddMonthsExample {
     public static void main(String[] args) {
+        // Get the current date
         LocalDate localDate = LocalDate.now();
+        
+        // Add 2 months to the current date
         LocalDate newDate = localDate.plusMonths(2);
+        
+        // Print the new date after adding 2 months
         System.out.println("New LocalDate After Adding 2 months: " + newDate);
     }
 }
